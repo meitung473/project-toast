270a85fb6be0e9cd570842cb2bc1ab1f22744392
meitung473 <a8870506@gmail.com>
2024-03-12 17:30:34 +0800

    Revert "Revert "Fix: Refactor useAutoDismiss & useTimelineDecoration to utilize data-* attributes""
    This reverts commit 1e7d24d4c997e850d2d0d00f54f0a5fcb8855e3a.

--- src/components/Toast/utils/useTimelineDecoration.js
+++ src/components/Toast/utils/useTimelineDecoration.js
@@ -1,16 +1,14 @@
 import React from "react";
-import * as styles from "./timeline.module.css";
-
+import "./timeline.module.css";
 function useTimelineDecoration(toastElement, { duration, color, delay }) {
     React.useEffect(() => {
         let toast = toastElement?.current;
         if (!toast) return;
-        toast.classList.add(styles["timeline"]);
+        if (!toast.getAttribute("data-timeline")) return;
         toast.style.setProperty("--timeline-delay", `${delay}ms`);
         toast.style.setProperty("--timeline-duration", `${duration}ms`);
-        toast.style.setProperty("--bg-color", color);
-
-        toast.setAttribute("data-running", "true");
+        toast.style.setProperty("--timeline-bg-color", color);
+        toast.setAttribute("data-timeline", "on");
     }, [color, duration, toastElement, delay]);
 }
 
