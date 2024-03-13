28d787db0defd3d222f43eb4ce00dc5e3f762c6c
meitung473 <a8870506@gmail.com>
2024-03-13 18:09:51 +0800

    Fix : fix image path in ToastPlayground <header>

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -23,7 +23,7 @@ function ToastPlayground() {
     return (
         <div className={styles.wrapper}>
             <header>
-                <img alt="Cute toast mascot" src="/toast.png" />
+                <img alt="Cute toast mascot" src="./toast.png" />
                 <h1>Toast Playground</h1>
             </header>
 
